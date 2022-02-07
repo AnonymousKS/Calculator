188615acab7383a60595b873271680c3121dbdc5
AnonymousKS <kunal26june@gmail.com>
2022-02-07 15:10:55 +0530

    final

--- script.js
+++ script.js
@@ -1,27 +1,122 @@
-function add(first, second) {
-    return first + second;
+let firstOperand = "";
+let secondOperand = "";
+let currentOperation = null;
+let resetFlag = false;
+
+const numberbtn = document.querySelectorAll(".numbers");
+const operatorbtn = document.querySelectorAll(".operator");
+const equalbtn = document.querySelector(".equal");
+const clearbtn = document.querySelector(".clear");
+const decimalbtn = document.querySelector(".decimal");
+const backspacebtn = document.querySelector(".backspace");
+const lastOperationScreen = document.getElementById("lastOperationScreen");
+const currentOperationScreen = document.getElementById(
+  "currentOperationScreen"
+);
+
+numberbtn.forEach((button) =>
+  button.addEventListener("click", () => displayNumber(button.textContent))
+);
+
+operatorbtn.forEach((button) =>
+  button.addEventListener("click", () => setOperator(button.textContent))
+);
+
+clearbtn.addEventListener("click", clear);
+backspacebtn.addEventListener("click", backspace);
+equalbtn.addEventListener("click", evaluate);
+decimalbtn.addEventListener("click", addDecimal);
+
+function displayNumber(number) {
+  if (currentOperationScreen.textContent === "0" || resetFlag) resetScreen();
+  currentOperationScreen.textContent += number;
 }
 
-function subtract(first, second) {
-    return first - second;
+function resetScreen() {
+  currentOperationScreen.textContent = "";
+  resetFlag = false;
 }
 
-function multiply(first, second) {
-    return first * second;
+function clear() {
+  currentOperationScreen.textContent = "0";
+  lastOperationScreen.textContent = "";
+  firstOperand = "";
+  secondOperand = "";
+  currentOperation = null;
 }
 
-function divide(first, second) {
-    if(second === 0) return 'Seriously, dude';
-    else 
-    return first / second;
+function backspace() {
+  currentOperationScreen.textContent = currentOperationScreen.textContent
+    .toString()
+    .slice(0, -1);
+  if (currentOperationScreen.textContent === "")
+    currentOperationScreen.textContent = "0";
 }
 
-function operate(first, operator, second) {
-    let result;
-    if(operator === '+') result = add(first, second);
-    else if(operator === '-') result = subtract(first, second);
-    else if(operator === '*') result = multiply(first, second);
-    else if(operator === '/') result = divide(first, second);
-    else return 'Please Select a valid operator';
-    return result;
+function setOperator(operator) {
+  if (currentOperation !== null) evaluate();
+  firstOperand = currentOperationScreen.textContent;
+  currentOperation = operator;
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation}`;
+  resetFlag = true;
+}
+
+function evaluate() {
+  if (currentOperation === null || resetFlag) return;
+  if (currentOperation === "/" && currentOperationScreen.textContent === "0") {
+    currentOperationScreen.textContent = "You dumb or what.";
+    return;
+  }
+  secondOperand = currentOperationScreen.textContent;
+  currentOperationScreen.textContent = roundResult(
+    operate(currentOperation, firstOperand, secondOperand)
+  );
+  lastOperationScreen.textContent = `${firstOperand} ${currentOperation} ${secondOperand} =`;
+  currentOperation = null;
+}
+
+function roundResult(number) {
+  return Math.round(number * 1000) / 1000;
+}
+
+function addDecimal() {
+  if (resetFlag) resetScreen();
+  if (currentOperationScreen.textContent === "")
+    currentOperationScreen.textContent = "0";
+  if (currentOperationScreen.textContent.includes(".")) return;
+  currentOperationScreen.textContent += ".";
+}
+
+function add(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
+
+function operate(operator, a, b) {
+  a = Number(a);
+  b = Number(b);
+  switch (operator) {
+    case "+":
+      return add(a, b);
+    case "-":
+      return subtract(a, b);
+    case "*":
+      return multiply(a, b);
+    case "/":
+      if (b === 0) return null;
+      else return divide(a, b);
+    default:
+      return null;
+  }
 }
